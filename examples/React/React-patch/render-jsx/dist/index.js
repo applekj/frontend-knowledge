2f879b4e00f970f3d25fe64dee8455736a881b46
严静杰 <yanjingjie@192.168.2.21>
2026-02-04 12:21:07 +0800

    add render-jsx

---
+++ examples/React/React-patch/render-jsx/dist/index.js
@@ -0,0 +1,58 @@
+// const vdom = {
+//     type: 'ul',
+//     props: {
+//         className: 'list'
+//     },
+//     children: [
+//         {
+//             type: 'li',
+//             props: {
+//                 className: 'item',
+//                 style: { background: 'blue', color: '#fff' },
+//                 onClick: function () { alert(1) }
+//             },
+//             children: [
+//                 'aaa'
+//             ]
+//         },
+//         {
+//             type: 'li',
+//             props: {
+//                 className: 'item',
+//             },
+//             children: [
+//                 'bbb'
+//             ]
+//         },
+//         {
+//             type: 'li',
+//             props: {
+//                 className: 'item',
+//             },
+//             children: [
+//                 'ccc'
+//             ]
+//         },
+//     ]
+// }
+const data = {
+  b: 'bbb',
+  c: 'ccc'
+};
+const vdom = createElement("ul", {
+  className: "list"
+}, createElement("li", {
+  className: "item",
+  style: {
+    background: 'blue',
+    color: '#fff'
+  },
+  onClick: function () {
+    alert(1);
+  }
+}, "aaa"), createElement("li", {
+  className: "item"
+}, data.b), createElement("li", {
+  className: "item"
+}, data.c));
+render(vdom, document.querySelector('#root'));
